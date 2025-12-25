10857209f2f644a0f05f8bba8081578613a38fc7
VoxXCode <devanoaghanzaputrapradessah@gmail.com>
2025-12-25 12:36:06 +0700

    Add gui-login

--- src/main/java/com/manajemenservis/main/Main.java
+++ src/main/java/com/manajemenservis/main/Main.java
@@ -1,15 +1,19 @@
 package com.manajemenservis.main;
 
+import com.manajemenservis.util.ExcelHandler; // Import Handler
 import com.manajemenservis.view.LoginView;
 import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        // Menjalankan UI di thread yang aman (Event Dispatch Thread)
+
+        // 1. Cek & Buat Database Excel Otomatis
+        ExcelHandler.pastikanFileAda();
+
+        // 2. Jalankan GUI
         SwingUtilities.invokeLater(() -> {
-            // Membuat instance LoginView dan menampilkannya
-            LoginView loginPage = new LoginView();
-            loginPage.setVisible(true);
+            LoginView loginView = new LoginView();
+            loginView.setVisible(true);
         });
     }
 }
