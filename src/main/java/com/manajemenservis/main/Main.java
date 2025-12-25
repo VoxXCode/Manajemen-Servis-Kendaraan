9810817e2811e200feb6fa18425d25ad717a4df8
VoxXCode <devanoaghanzaputrapradessah@gmail.com>
2025-12-25 14:18:22 +0700

    Update remove-comment

--- src/main/java/com/manajemenservis/main/Main.java
+++ src/main/java/com/manajemenservis/main/Main.java
@@ -7,10 +7,8 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
 
-        // 1. Cek & Buat Database Excel Otomatis
         ExcelHandler.pastikanFileAda();
 
-        // 2. Jalankan GUI
         SwingUtilities.invokeLater(() -> {
             LoginView loginView = new LoginView();
             loginView.setVisible(true);
